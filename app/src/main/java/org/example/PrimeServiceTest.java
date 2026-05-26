4ee8abea64ccdf6f14bfb2c211db8b38a23734f3
stu249360 <stu249360@mail.uni-kiel.de>
2026-05-26 14:39:42 +0200

    jo

---
+++ app/src/main/java/org/example/PrimeServiceTest.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class PrimeServiceTest {
+
+    @Test
+    void mockTest() {
+
+        PrimeService service =
+            mock(PrimeService.class);
+
+        when(service.pruefe(7))
+            .thenReturn(true);
+
+        assertTrue(
+            service.pruefe(7)
+        );
+    }
+}
